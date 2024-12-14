4c0d85ba461fbf14b2afc22e1613732482f94ed5
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2024-12-14 13:54:27 -0600

    initial navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import NavBar from "./components/NavBar";
+
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello World!
-        </h1>
+        <>
+            <NavBar />
+            <p className="text-center text-green-500 font-bold">
+                Hey Guys!
+            </p>
+        </>
+
     )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import {ReactNode, useState, useEffect } from 'react';
+// @ts-ignore
+import { IconName } from 'react-icons/fa';
+
+
+
+
+const NavBar = () => {
+  return (
+    <div className='fixed top-0 h-screen w-16 m-0 flex flex-row bg-emerald-700 text-black shadow'>
+      <i>A</i>
+        <i>B</i>
+        <i>C</i>
+        <i>D</i>
+    </div>
+  )
+}
+
+export default NavBar;
