c88b1fd87a7cc0dc1ff2e53111b36b4d335007ba
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2024-12-31 00:11:56 -0600

    fully circular image and left align experience text

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,8 +3,8 @@ import logo from '../assets/Belmont-University-Logo-Vertical-Reversed.png'
 
 const Experience = () => {
     return (
-        <div className="justify-center flex flex-col w-full text-left mx-auto text-black dark:text-white mt-[50vh] px-8 lg:px-36">
-            <span className="text-5xl  md:text-6xl  justify-center font-medium pb-4">
+        <div className="justify-center flex flex-col w-full text-left mx-auto text-black dark:text-white mt-[40vh] px-8 lg:px-36">
+            <span className="text-5xl md:text-6xl justify-center font-medium pb-16">
                 Here is my <span className="text-[#46d85b] font-bold">experience</span>
             </span>
             <div className="pb-4 flex flex-row items-center">
@@ -15,19 +15,17 @@ const Experience = () => {
                     className="group relative flex flex-row items-center gap-4"
                 >
                     <img
-                            src={logo}
-                            alt="Belmont University Logo"
-                            className="object-scale-down max-h-28"
+                        src={logo}
+                        alt="Belmont University Logo"
+                        className="object-scale-down max-h-32 rounded-full"
                     />
                     <div className="flex flex-col pl-1">
-                        <h2
-                            className="text-black dark:text-white font-semibold text-xl pb-2">
+                        <h2 className="text-black dark:text-white font-semibold text-xl pb-2">
                             Research Intern, Belmont University
                         </h2>
-                        <h3 className="text-black dark:text-white text-lg">
+                        <h3 className="text-black dark:text-white text-lg max-w-[24rem] sm:max-w-[20rem] md:max-w-[22rem] lg:max-w-[28rem]">
                             Experimented with a Python-based simulation tool that helps to understand how light
-                            interacts
-                            with tiny particles, making cutting-edge research tools more accessible to
+                            interacts with tiny particles, making cutting-edge research tools more accessible to
                             university students.
                         </h3>
                     </div>
